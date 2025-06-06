55e678901e6d360eadc3d3c6ad7d2c8c9557acae
lasse-cs <lasse.schmieding@gmail.com>
2025-06-06 22:18:14 +0100

    Initial commit

---
+++ shopping/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+
+class ShoppingList(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        User,
+        related_name="shopping_lists",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"{self.name} - owned by {self.user.username}"
+
+
+class ShoppingItem(models.Model):
+    shopping_list = models.ForeignKey(
+        ShoppingList,
+        related_name="items",
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=100)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
